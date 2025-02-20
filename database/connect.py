3566062e5677bbd98498719a35e3575c11cd84c3
Omar Mehenni <2024_omar.mehenni@iticbcn.com>
2025-02-20 10:07:25 +0100

    connect.py acabat

---
+++ database/connect.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+def connection_db():
+    conn = psycopg2.connect(
+        database="the_bear",
+        password="admin",
+        user="admin",
+        host="localhost",
+        port="5432"
+        )
+
+    return conn
+
+connect = connection_db()
+print(connect)
+connect.close()
+print(connect)
+
