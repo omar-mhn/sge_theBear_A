74cdf89c32f5a4df4a8b5146e89a41cdfb5a9dd8
mariapalomeque <2023_maria.palomeque@iticbcn.cat>
2025-03-13 12:26:15 +0100

    Codigo actividad FASTAPI 2

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
 # sge_theBear_A
-## URL 
+
+## PRACTICA 6 
+### URL 
 ![alt text](image-1.png)
 
-## Funcionamiento del endpoint utilizando Swagger
+### Funcionamiento del endpoint utilizando Swagger
 ![alt text](image.png)
 
 
 
-## Endpoint con muestra d einformación de los profesores
+### Endpoint con muestra d einformación de los profesores
 
 ![alt text](image-3.png)
 
-## Vista de la terminal 
+### Vista de la terminal 
 
 ![alt text](image-2.png)
+
+##  PRACTICA 7 
+
+### Instalación de dependencias 
+
+![alt text](image-4.png)
+
+CAPTURA NUEVA TABLA USER 
+
+A partir del modelo de la tabla que hemos creado en la clase User.py, en la clase main importamos dicho modelo, generamos la conexion con la base de datos, y 
--- main.py
+++ main.py
@@ -2,9 +2,44 @@ from typing import List
 from fastapi import FastAPI
 from services import read
 
+# Ampliación imports
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, create_engine, Session
+from detenv import load_dotenv
+from services import user
+import os
+
 app = FastAPI()
 
 @app.get(path="/root", response_model=List[dict])
 async def read_root() :
     result = read.registre()
+    return result
+
+#Cargar variables de entorno
+load_dotenv()
+
+#   Configuraci´-on conexion postgresSQL
+DATABASE_URL = os.getenv("DATABASE_URL") # Obtener url conexion desde .env
+engine =create_engine(DATABASE_URL) #Crear engine de conexion
+
+#Crear las tablas de la base de datos 
+SQLModel.metadata.create_all(engine)
+
+#
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/users/", response_model=list[dict])
+def read_user(db:Session = Depends(get_db)):
+    result =user.get_all_users(db)
+    return result
+
+@app.post("/users/", response_model=dict)
+def create_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
     return result
